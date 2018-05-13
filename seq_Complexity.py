86966c425cf59277ec20a702919dc4c1345b0f72
Mamoon Ismail <Mamoon_Ismail@my.uri.edu>
2018-05-13 14:19:26 -0400

    modify the script

--- seq_Complexity.py
+++ seq_Complexity.py
@@ -1,3 +1,72 @@
+#!/usr/bin/env python3
+
+import glob
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+#import seaborn as sns
+import sys
+
+#function to count the kmers
+def count_kmers(seq, k):
+    counts = {}
+    for i in range(len(seq) - k + 1):
+        kmer = seq[i:i+k]
+        if kmer not in counts:
+            counts[kmer] = 0
+        counts[kmer] += 1
+    return counts
+
+#function to find the wrong DNA
+def check_seq_format(seq):
+    bad_chars = {}
+    for i in seq:
+        if i not in 'AGCT':
+            if i in bad_chars: bad_chars[ i ] += 1
+            else: bad_chars[ i ] = 1
+    if bad_chars != {}: return bad_chars
+
+#function to export a table(kmers, observed, possible) in .csv format per each sequence name
+def generate_file(file_name, detail_data, seq_name):
+    data = pd.DataFrame([[i[0], i[1], i[2]] for i in detail_data], columns=['k-mers', 'observed', 'possible'])
+    #print('data-frame',detail_data)
+    data.to_csv('output/data/'+ file_name + '_' + seq_name+'.csv', index=False)
+    #return data
+
+#function to generate a linguistic complexity graph of all sequence name
+def generate_complexity_graph(file_name, seq_name_list, linguistic_complexity_list):       
+    plt.plot(seq_name_list, linguistic_complexity_list, 'ro')
+    plt.xlabel('Sequence Name')
+    plt.ylabel('Linguistic Complexity')
+    #plt.show()
+    graph_name = file_name + '_complexity.png'
+    plt.savefig('output/image/'+graph_name)
+    #return data
+
+#function to generate comparison of possible and observed kmers of all sequence name
+def generate_kmers_graph(file_name, seq_name_list, possible_total_list, observed_total_list):
+    fig, ax = plt.subplots()
+    index = np.arange(len(seq_name_list))
+    bar_width = 0.35
+    opacity = 0.8
+    rects1 = plt.bar(index, possible_total_list, bar_width,
+                     alpha=opacity,
+                     color='b',
+                     label='Possible Kmers')
+    rects2 = plt.bar(index + bar_width, observed_total_list, bar_width,
+                     alpha=opacity,
+                     color='g',
+                     label='Observed Kmers')
+    plt.xlabel('Sequence Names')
+    plt.ylabel('Kmers')
+    plt.title('Kmers per each Sequence Name')
+    plt.xticks(index + bar_width, seq_name_list)
+    plt.legend()
+    plt.tight_layout()
+    #plt.show()
+    graph_name = file_name + '_kmers.png'
+    plt.savefig('output/image/'+graph_name)
+
 #function to generate wrong dana per each sequence name 
 def generate_wrong_dna_graph(file_name, data, seq_name):
     plt.clf()
